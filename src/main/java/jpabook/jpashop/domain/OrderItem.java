8d80d574a2509a0c36d91bc627fade20ca480d9f
Egenieee <hhgg0925@naver.com>
2023-03-01 14:06:14 +0900

    [FEAT] Order 관련 엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne // 상품 하나를 여러 개 주문할 수 있으니까
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
