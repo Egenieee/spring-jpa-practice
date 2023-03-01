0849e461199d9ab4051bf71fc277998647398701
Egenieee <hhgg0925@naver.com>
2023-03-01 17:18:23 +0900

    [CHORE] FetchType을 LAZY로 설정

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -15,11 +15,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne // 상품 하나를 여러 개 주문할 수 있으니까
+    @ManyToOne(fetch = FetchType.LAZY) // 상품 하나를 여러 개 주문할 수 있으니까
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
