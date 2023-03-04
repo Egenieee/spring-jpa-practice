6236247c80c2ab1547092954e7034489199df249
Egenieee <hhgg0925@naver.com>
2023-03-04 16:54:17 +0900

    [TEST] MemberServiceTest 코드 작성
    - 회원 가입 test
    - 중복 회원 예외 test

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//@WebAppConfiguration
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("lee");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member memberOne = new Member();
+        memberOne.setName("lee");
+
+        Member memberTwo = new Member();
+        memberTwo.setName("lee");
+
+        //when
+        memberService.join(memberOne);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> memberService.join(memberTwo));
+    }
+
+}
